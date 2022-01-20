6e20e6b8ba180a072702f227ce0662b5ed22a70e
TP1997 <thanhphamlike@gmail.com>
2022-01-20 16:34:38 -0500

    Added image slider

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 # This is a small project for npm publishment practice
 
 ## Purpose of module:
-Used to create a dropdown menu, and many more later
+Used to create a dropdown menu, image slider, and many more later
 
 ## Usage
 
-Install
+### Install
 ```
 npm install quickitrx
 ```
-Usage
+### Usage
 ```
 import interactiveModule from 'quickitrx/src/interactiveModule';
 
-let dropdown = interactiveModule.dropdownMenu(headerObj, itemObjArray);
 ```
 
 ```
+__dropdownMenu
+let dropdown = interactiveModule.dropdownMenu(headerObj, itemObjArray);
+
 let dropdown = dropdownMenu({
   {header: 'This is your dropdown header'},
   [{
@@ -31,5 +33,15 @@ let dropdown = dropdownMenu({
 
 navbarNode.appenchild(dropdown);
 ```
+```
+__imgSlider
+let imgSlider = imgSlider(imageUrlArray, widthPx = 300, timeoutMs = 1000);
 
+let imgSlider = imgSlider([
+  url1, url2, url3, ...
+], 300, 1000);
+let imgSlider = imgSlider(imageUrlArray, width = 300, timeoutMs = 1000);
+navbarNode.appenchild(imgSlider);
+
+```
 ### Note that: if you dont spicify headerOb or/and itemObjArray, default values will be used.
--- src/interactiveModule.js
+++ src/interactiveModule.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-const interactiveModule = (function(){
+const interactiveModule = (function () {
 
   /**
    * 
@@ -9,7 +9,7 @@ const interactiveModule = (function(){
    * text: string},...]} itemsArr 
    * @returns 
    */
-  const dropdownMenu = (headerObj = {}, itemsArr =[]) => {
+  const dropdownMenu = (headerObj = {}, itemsArr = []) => {
     const container = document.createElement('div');
     container.className = 'dropdown';
     const header = document.createElement('h2');
@@ -47,7 +47,70 @@ const interactiveModule = (function(){
     return container;
   }
 
-  return {dropdownMenu};
+  const imageSlider = (imageUrlArray, width = 300, timeoutMs = 1000) => {
+    if (imageUrlArray.length === 0) return;
+    let curImagePos = 0;
+    const sliderContainer = document.createElement('div');
+    const slider = document.createElement('div');
+    const slides = [];
+    const backwardBtn = document.createElement('div');
+    const forwardBtn = document.createElement('div');
+    sliderContainer.classList.add('sliderContainer');
+    sliderContainer.style.width = `${width}px`;
+    slider.classList.add('slider');
+    backwardBtn.classList.add('backward');
+    forwardBtn.classList.add('forward');
+    const backwardIcon = document.createElement('span');
+    backwardIcon.textContent = '<';
+    const forwardIcon = document.createElement('span');
+    forwardIcon.textContent = '>';
+
+    backwardBtn.appendChild(backwardIcon);
+    forwardBtn.appendChild(forwardIcon);
+    imageUrlArray.forEach((url, index) => {
+      let slide = document.createElement('div');
+      let img = new Image();
+      img.id = index;
+      img.src = url;
+      img.alt = 'No image';
+      img.style.width = `${width}px`;
+      slide.classList.add('slide');
+      slide.appendChild(img);
+      slider.appendChild(slide);
+      slides.push(slide);
+    });
+    slides.forEach(slide => {
+      slide.style.opacity = 0;
+    })
+    slides.at(curImagePos).style.opacity = 1;
+    backwardBtn.addEventListener("click", () => {
+      slides.at(curImagePos).style.opacity = 0;
+      curImagePos === 0 ? curImagePos = imageUrlArray.length - 1 : curImagePos--;
+      slider.style.transform = `translateX(${-curImagePos * width}px)`;
+      slides.at(curImagePos).style.opacity = 1;
+    })
+    forwardBtn.addEventListener("click", () => {
+      slides.at(curImagePos).style.opacity = 0;
+      curImagePos === (imageUrlArray.length - 1)? curImagePos = 0 : curImagePos++;
+      slider.style.transform = `translateX(${-curImagePos * width}px)`;
+      slides.at(curImagePos).style.opacity = 1;
+    })
+    sliderContainer.appendChild(slider);
+    sliderContainer.appendChild(backwardBtn);
+    sliderContainer.appendChild(forwardBtn);
+    setInterval(() => {
+      slides.at(curImagePos).style.opacity = 0;
+      curImagePos === (imageUrlArray.length - 1) ? curImagePos = 0 : curImagePos++;
+      slider.style.transform = `translateX(${-curImagePos * width}px)`;
+      slides.at(curImagePos).style.opacity = 1;
+    }, timeoutMs);
+
+    return sliderContainer;
+  }
+
+
+  return { dropdownMenu, imageSlider };
 })();
+
 
 export default interactiveModule;
